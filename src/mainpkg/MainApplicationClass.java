9a8fef23f781b7f27da09fb747c9bd66c144c703
Nafiz Ahmed Sargis <124522412+sargis02@users.noreply.github.com>
2024-03-26 22:32:39 +0600

    improve

--- src/mainpkg/MainApplicationClass.java
+++ src/mainpkg/MainApplicationClass.java
@@ -11,26 +11,28 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
+ * The main class of the application, which launches the JavaFX runtime and displays the primary stage.
  *
- * @author User
+ * @author your-name-here
  */
-public class MainApplicationClass extends Application {
-    
+public class MainApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("StudentInputOutputScene.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     /**
+     * The entry point of the application.
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
